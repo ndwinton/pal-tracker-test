47871dbbf6885766b7635fdae11e5ef8c4737530
Neil Winton <nwinton@pivotal.io>
2020-02-11 10:55:09 +0000

    Environment lab completed

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String message;
+
+    public WelcomeController(@Value("${welcome.message}") String message) {
+        this.message = message;
+    }
+
     @GetMapping("/")
-    String greet() {
-        return "hello";
+    public String sayHello() {
+        return message;
     }
 }
